ff635772ff51ad20c6ce626ef0204c354f7cddca
stel-s <stelios.sideras@gmail.com>
2017-10-03 20:55:23 +0300

    make it better

--- server.js
+++ server.js
@@ -5,4 +5,11 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT ss || 8080);
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+app.listen(process.env.PORT  || 8080);
